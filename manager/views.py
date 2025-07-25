d4d0321734d4e9c3bcf046880fdad218eba8cf99
idrice junior <dongmojunior18@gmail.com>
2025-07-25 16:15:41 -0400

    function

--- manager/views.py
+++ manager/views.py
@@ -1,7 +1,43 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+from .models import Post,Profile,Comment,User
+from django.urls import reverse
+from django.contrib import auth
+from django.contrib.auth.hashers import make_password
+import itertools
+
 
 # Create your views here.
 
 def index(request):
     return render(request,"manager/index.html")
+
+def verifyInput(username,password):
+    error = []
+    if len(username) < 4:
+        error.append("Username should be atleast four characters")
+    elif len(password)<8:
+        error.append('password should be atleast eight characters')
+    else:
+        print("perfect")
+    return error
+
+def registerUser(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        hashed_password = make_password(password)
+
+        error =verifyInput(username, password)
+        if len(error) != 0:
+            error = error[0]
+            print(error)
+            return render(request, 'manager/registration_form.html',{
+                "error":error
+            })
+        else:
+         a = User(username=username, email=email, password=password)
