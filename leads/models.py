c1379e80abd370ffdd5dc4b1c64214c9beb4cbf6
idrice junior <dongmojunior18@gmail.com>
2025-07-29 15:09:02 -0400

    leads

--- leads/models.py
+++ leads/models.py
@@ -10,9 +10,15 @@ class Lead(models.Model):
     last_name = models.CharField(max_length=40)
     age = models.IntegerField(default=0)
 
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+
     '''phoned = models.BooleanField(default=False)
     source = models.CharField(choices=SOURCE_CHOICES, max_length=100)
     profile_picture = models.ImageField(blank=True, null=True)
     file = models.FileField(blank=True, null=True)'''
 
     #FOREIGN KEY ISSUES
+
+class Agent(models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
