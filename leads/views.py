796582c8c50950666ea260efa62552f2c9c5a853
idrice junior <dongmojunior18@gmail.com>
2025-08-04 16:02:18 -0400

    all

--- leads/views.py
+++ leads/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+from .models import Lead
 
 def home_page(request):
+    leads = Lead.objects.all()
     context = {
-        "name":"idrice",
-        "age":22
+        "names":leads
     }
     return render(request, 'base.html', context)
