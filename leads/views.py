c72b94f38a6f62f6dfc31fe642173de8ac800f92
idrice junior <dongmojunior18@gmail.com>
2025-08-04 20:40:21 -0400

    add

--- leads/views.py
+++ leads/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 # Create your views here.
-from .models import Lead
+from .models import Lead,Agent
 from .forms import LeadForm
 
 def home_page(request):
@@ -21,7 +21,26 @@ def lead_details(request, pk):
     return render(request, 'leads/details.html',context)
 
 def lead_create(request):
+    form = LeadForm()
+    if request.method == "POST":
+        print("received")
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            print("form valid")
+            print(form.cleaned_data)
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            age = form.cleaned_data['age']
+            agent = Agent.objects.first()
+            Lead.objects.create(
+                first_name = first_name,
+                last_name = last_name,
+                age = age,
+                agent = agent
+            )
+            print("lead created")
+            return redirect('/')
     context = {
-        "form":LeadForm()
+        "form":form
     }
     return render(request, "leads/lead_create.html",context)
