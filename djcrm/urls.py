6512671019180219ee15fd320f2a1b90fa71f89b
idrice junior <dongmojunior18@gmail.com>
2025-08-07 11:46:26 -0400

    send emails

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -17,11 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from leads.views import LadingPage
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("leads.urls", namespace="leads"))
+    path('',include("leads.urls", namespace="leads")),
+    
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
 
 #CRUD+L : CREATE RETRIERVE UPDATE AND DELETE VIEWS+LIST
